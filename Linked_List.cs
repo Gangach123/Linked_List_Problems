35a86d2dddbd1032952ca857441a93220d0aae22
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-01 21:14:13 +0530

    Creating_Elements

---
+++ Linked_List.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_Problem
+{
+    internal class Linked_List
+    {
+        public Node head;
+
+        //To Creating elements to the list
+        public void CreatingElements(int value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+
+            }
+        }
+        public void DisplayLinkedList()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    Console.WriteLine($"{temp.data}");
+                    temp = temp.next;
+                }
+                Console.WriteLine($"{temp.data}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-namespace Linked_List
+namespace Linked_List_Problem
 {
     class Program
     {
         public static void Main()
         {
             Console.WriteLine("Welcome to Linked_List_Problems");
+            Linked_List linked_List = new Linked_List();
+            linked_List.CreatingElements(56);
+            linked_List.CreatingElements(30);
+            linked_List.CreatingElements(70);
+            linked_List.DisplayLinkedList();
         }
     }
 }
