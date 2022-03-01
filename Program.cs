393cd8ccee701ec73f8623adddc2e2c0f77a8367
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-03-01 20:52:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Linked_List
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Linked_List_Problems");
+        }
+    }
+}
